f7ec97d5019723e91b8c55af57b6dcfd3a418d9c
Xanonymous <trusaidlin@gmail.com>
2023-11-08 03:52:55 +0000

    feat: 🎸 exercise 3 problem 1

---
+++ src/exercise_3/main.py
@@ -0,0 +1,34 @@
+from data.uci import retrieve_uci_data
+from equations import calculate_linear_regression_parameters
+from model.uciml import UciMLRepo
+from plot import plot_points, plot_line, show_plot
+
+
+def run_problem1(*, dataset: UciMLRepo) -> None:
+    data = dataset.data.features
+    length = data['Length'].to_numpy()
+    diameter = data['Diameter'].to_numpy()
+    m, b = calculate_linear_regression_parameters(x_series=length, y_series=diameter)
+
+    plot_points(
+        x_series=length,
+        y_series=diameter,
+        x_axis_label='Length',
+        y_axis_label='Diameter',
+        point_size=3,
+    )
+    plot_line(
+        slope=m,
+        intercept=b,
+        line_size=3,
+    )
+    show_plot(title='Length vs Diameter', should_show_grid=True)
+
+
+def run() -> None:
+    abalone = retrieve_uci_data(repo_name='abalone')
+    run_problem1(dataset=abalone)
+
+
+if __name__ == '__main__':
+    run()
