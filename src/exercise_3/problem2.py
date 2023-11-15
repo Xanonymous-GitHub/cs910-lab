cbccf30e3c382c7c85068beaf7468b845c6f9b0e
Xanonymous <trusaidlin@gmail.com>
2023-11-15 00:03:55 +0000

    feat: 🎸 done problem 2

---
+++ src/exercise_3/problem2.py
@@ -0,0 +1,25 @@
+from data.uci import retrieve_uci_data
+from model.uciml import UciMLRepo
+from plot.stats import create_regression_stat_model
+
+
+def run_problem2(*, dataset: UciMLRepo) -> None:
+    data = dataset.data.features
+
+    model = create_regression_stat_model(
+        data,
+        'Whole_weight',
+        'Shucked_weight',
+        'Viscera_weight',
+        'Shell_weight',
+    )
+    print(model.summary(slim=True))
+
+
+def run() -> None:
+    abalone = retrieve_uci_data(repo_name='abalone')
+    run_problem2(dataset=abalone)
+
+
+if __name__ == '__main__':
+    run()
