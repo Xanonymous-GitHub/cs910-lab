a1a3b224fbda02b7bd6cc858591943e8c87d2bbd
Xanonymous <trusaidlin@gmail.com>
2023-11-15 01:52:23 +0000

    feat: 🎸 finish problem 4

---
+++ src/exercise_3/problem4.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from data.uci import retrieve_uci_data
+from model.uciml import UciMLRepo
+from plot.stats import create_logistic_stat_model
+
+
+def run_problem4(*, dataset: UciMLRepo) -> None:
+    data = dataset.data.original
+
+    # TODO: use .loc
+    data['Sex'] = np.where(data['Sex'] == 'I', 1, 0)
+
+    model_a = create_logistic_stat_model(
+        data,
+        'Sex ~ Length',
+    )
+    print(model_a.summary(title='logit(Sex) = a * Length + b'))
+
+    model_b = create_logistic_stat_model(
+        data,
+        'Sex ~ Whole_weight',
+    )
+    print(model_b.summary(title='logit(Sex) = a * Whole_weight + b'))
+
+    model_c = create_logistic_stat_model(
+        data,
+        'Sex ~ Rings',
+    )
+    print(model_c.summary(title='logit(Sex) = a * Rings + b'))
+
+    model_d = create_logistic_stat_model(
+        data,
+        'Sex ~ Length + Whole_weight + Rings',
+    )
+    print(model_d.summary(title='logit(Sex) = a * Length + b * Whole_weight + c * Rings + d'))
+
+
+def run() -> None:
+    abalone = retrieve_uci_data(repo_name='abalone')
+    run_problem4(dataset=abalone)
+
+
+if __name__ == '__main__':
+    run()
