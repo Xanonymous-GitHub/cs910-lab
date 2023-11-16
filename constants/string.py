0a8e2eb5f38cbcd4cf94da507df3fa2a0034a913
Xanonymous <trusaidlin@gmail.com>
2023-11-16 05:39:42 +0000

    refactor: 💡 remove unneeded modules

--- constants/string.py
+++
@@ -1,6 +0,0 @@
-from typing import Final
-
-from utils.path import runtime_path_resolver
-
-RUNTIME_DIR: Final[str] = runtime_path_resolver.RUNTIME_DIR
-CACHE_FILE_EXTENSION: Final[str] = "cache"
--- utils/pipeline.py
+++
@@ -1,9 +0,0 @@
-from collections.abc import Callable
-
-
-def skip(func: Callable) -> Callable:
-    def wrapper(*_, **__):
-        print(f"Skipping {func.__name__}...")
-        pass
-
-    return wrapper
