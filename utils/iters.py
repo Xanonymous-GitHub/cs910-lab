aa6ff90d5db527e0f2bd78fcb698953b48c3e1ef
Xanonymous <trusaidlin@gmail.com>
2023-11-20 12:27:00 +0000

    refactor: 💡 extend combination func

--- utils/iters.py
+++ utils/iters.py
@@ -2,8 +2,15 @@ from itertools import combinations
 from typing import Sequence, Generator
 
 
-def create_combinations_from[T](items: Sequence) -> Generator[tuple[T, ...], ..., ...]:
+def create_combinations_from[T](
+        items: Sequence,
+        /,
+        *,
+        min_length: int = 1,
+        max_length: int = None,
+) -> Generator[tuple[T, ...], ..., ...]:
     length = len(items)
-    for cell_length in range(1, length + 1):
+    max_length = max_length or length
+    for cell_length in range(min_length, max_length + 1):
         for combination in combinations(items, cell_length):
             yield combination
